fd84f1b7feb3d00abc363ab1a6c8f4914aabfefb
Julie Lee <leejulie09@gmail.com>
2022-05-25 15:37:09 +0900

    Add: first commit. 초기세팅완료.

---
+++ src/Router.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import LoginBokyung from "./pages/bokyungKim/Login/Login";
+import MainBokyung from "./pages/bokyungKim/Main/Main";
+
+import LoginJulie from "./pages/Julie/Login/Login";
+import MainJulie from "./pages/Julie/Main/Main";
+
+import LoginSangbin from "./pages/Sangbin/Login/Login";
+import MainSangbin from "./pages/Sangbin/Main/Main";
+
+import LoginSanghyeon from "./pages/Sanghyeon/Login/Login";
+import MainSanghyeon from "./pages/Sanghyeon/Main/Main";
+
+import LoginShinhye from "./pages/Shinhye/Login/Login";
+import MainShinhye from "./pages/Shinhye/Main/Main";
+
+import LoginSunghee from "./pages/Sunghee/Login/Login";
+import MainSunghee from "./pages/Sunghee/Main/Main";
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login-bokyung" element={<LoginBokyung />} />
+        <Route path="/main-bokyung" element={<MainBokyung />} />
+
+        <Route path="/login-julie" element={<LoginJulie />} />
+        <Route path="/main-julie" element={<MainJulie />} />
+
+        <Route path="/login-sangbin" element={<LoginSangbin />} />
+        <Route path="/main-sangbin" element={<MainSangbin />} />
+
+        <Route path="/login-sanghyeon" element={<LoginSanghyeon />} />
+        <Route path="/main-sanghyeon" element={<MainSanghyeon />} />
+
+        <Route path="/login-shinhye" element={<LoginShinhye />} />
+        <Route path="/main-shinhye" element={<MainShinhye />} />
+
+        <Route path="/login-sunghee" element={<LoginSunghee />} />
+        <Route path="/main-sunghee" element={<MainSunghee />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
